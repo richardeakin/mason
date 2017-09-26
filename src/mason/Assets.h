b9dd05852db32e054edbfdb21da1b258917b2f11
Rich E <rich.eakin@gmail.com>
2017-09-26 14:41:43 -0400

    typo fix

--- src/mason/Assets.h
+++ src/mason/Assets.h
@@ -158,7 +158,7 @@ public:
 
 	//! Returns the requested texture within the provided \a updateCallback upon initial load and any time it is updated on file. Loads synchronously if the texture is not cached.
 	ci::signals::Connection getTexture( const ci::fs::path &texturePath, const std::function<void( ci::gl::Texture2dRef )> &updateCallback  );
-	//! Calls \a updateCallback whenever the file at \a path is modified and needs to be relaoded. Returns a WatchRef to handle the scope of the associated file watch (empty in deploy mode)
+	//! Calls \a updateCallback whenever the file at \a path is modified and needs to be reloaded. Returns a WatchRef to handle the scope of the associated file watch (empty in deploy mode)
 	ci::signals::Connection getFile( const ci::fs::path &path, const std::function<void( ci::DataSourceRef )> &updateCallback );
 	//! Loads and returns a DataSourceRef for the file associated with \a path.
 	ci::DataSourceRef loadAsset( const ci::fs::path &path );
