170c511360955e3d021f28bad3bac49abbaa5a64
Richard Eakin <rich.eakin@gmail.com>
2018-11-21 21:23:49 -0500

    removed older Config impl

--- src/mason/Config.cpp
+++ src/mason/Config.cpp
@@ -10,8 +10,6 @@ using namespace std;
 
 namespace mason {
 
-#if 1
-
 namespace detail {
 
 static ma::Dictionary sConfig;
@@ -41,90 +39,5 @@ ma::Dictionary*	config()
 {
 	return &detail::sConfig;
 }
-
-#else
-
-// static
-Config* Config::instance()
-{
-	static Config sInstance;
-	return &sInstance;
-}
-
-bool Config::read( const DataSourceRef &source )
-{
-	mSource = source;
-
-	if( mSource->isFilePath() ) {
-		mTarget = DataTargetPath::createRef( mSource->getFilePath() );
-	}
-	else {
-		// We can only write to files on disk.
-		mTarget.reset();
-	}
-
-	try {
-		string data = loadString( source );
-	
-		Json::Reader reader;
-		bool success = reader.parse( data, mRoot );
-		if( ! success ) {
-			string errorMessage = reader.getFormattedErrorMessages();
-			throw ConfigExc( "Json::Reader failed to parse source, error message: " + errorMessage );
-		}
-
-		mDirty = false;
-	}
-	catch( std::exception &exc ) {
-		CI_LOG_EXCEPTION( "failed to read source", exc );
-		return false;
-	}
-
-	return true;
-}
-
-bool Config::write( const DataTargetRef &target )
-{
-	if( target ) {
-		const fs::path &path = target->getFilePath();
-		if( ! fs::exists( path ) ) {
-			fs::create_directories( path.parent_path() );
-			mDirty = true;
-		}
-
-		if( mDirty ) {
-			Json::StyledWriter writer;
-			string data = writer.write( mRoot );
-
-			ofstream ostream( path.c_str() );
-			ostream << data;
-			ostream.close();
-		}
-
-		return true;
-	}
-
-	return false;
-}
-
-fs::path Config::getTargetFilePath() const
-{
-	if( ! mTarget )
-		return {};
-
-	return mTarget->getFilePath();
-}
-
-ma::Dictionary Config::getGroupAsDictionary( const std::string &category ) const
-{
-	if( ! mRoot.isMember( category ) ) {
-		throw ConfigExc( "no category with name: " + category );
-	}
-
-	const auto &group = getGroup( category );
-	return Dictionary::convert( group );
-}
-
-#endif
 
 } // namespace mason
